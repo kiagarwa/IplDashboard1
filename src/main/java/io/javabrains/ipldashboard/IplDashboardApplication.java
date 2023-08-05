f399bf9a3009d066731a9af7713c6e947d06e425
Kishan Agarwalla <kiagarwa@akamai.com>
2023-08-05 10:43:26 +0530

    Initial commit

---
+++ src/main/java/io/javabrains/ipldashboard/IplDashboardApplication.java
@@ -0,0 +1,20 @@
+package io.javabrains.ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+
+	SpringApplication.run(IplDashboardApplication.class, args);
+
+
+
+
+
+	}
+
+}
